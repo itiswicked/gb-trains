bcb10ad192aef740cbaaa755fc54f3007d10eef0
Nathan Bourke <nathanrbourke@gmail.com>
2023-01-09 12:13:30 -0500

    remove pry

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
   def index
     stations = stations = GbTrainNetwork::Station.all
     segments = GbTrainNetwork::SegmentCalculator.new(stations).segments
-# require 'pry';binding.pry
+
     render json: { stations: stations, segments: segments }
   end
 end
